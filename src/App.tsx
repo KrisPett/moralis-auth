a26d89896ef15800a37fe7898b2f1256c8e40b28
Kristoffer Pettersson <kristoffer.pettersson@sensera.se>
2022-03-16 20:22:45 +0100

    Push

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <ThemeProvider theme={mainTheme}>
             <Root/>
-            {/*testsss*/}
+            {/*testssffs*/}
         </ThemeProvider>
     );
 }
